13c004178ad0451134117fd6d84a5643cdb77ad1
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-09-02 17:47:01 +0700

    console log client info

--- public/js/client.js
+++ public/js/client.js
@@ -4,6 +4,9 @@ window.addEventListener("DOMContentLoaded", () => {
     ws = new WebSocket(`ws://localhost:3000/ws`);
     ws.addEventListener("open", onConnectionOpen);
     ws.addEventListener("message", onMessageReceived);
+
+    const queryParams = getQueryParams();
+    console.log(queryParams);
 });
 
 function onConnectionOpen(){
@@ -13,3 +16,16 @@ function onConnectionOpen(){
 function onMessageReceived(event){
     console.log('Message Received ', event);
 }
+
+
+function getQueryParams() {
+    const search = window.location.search.substring(1);
+    const pairs = search.split("&");
+    const params = {};
+    for (const pair of pairs) {
+      const parts = pair.split("=");
+      params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+    }
+  
+    return params;
+  }
