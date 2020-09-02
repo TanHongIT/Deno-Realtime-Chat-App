578d209480fd8a9fa9572ed3c56b82aff87f017a
Nguyễn Phương Tân <phuongtan12357nguyen@gmail.com>
2020-09-02 22:57:26 +0700

    show Message Received

--- chat.js
+++ chat.js
@@ -1,11 +1,70 @@
 import { v4 } from "https://deno.land/std/uuid/mod.ts"
 
+/**
+ * userId: {
+ *      userId: string,
+ *      name: string,
+ *      groupName: string,
+ *      ws: WebSocket
+ * }
+ */
+
+const usersMap = new Map();
+
+/**
+ * groupName: [user1, user2]
+ * 
+ * {
+ *      userId: string,
+ *      name: string,
+ *      groupName: string,
+ *      ws: WebSocket
+ * }
+ */
+
+const groupsMap = new Map();
+
 export default async function chat(ws){
     console.log('Connected');
 
     const userId = v4.generate();
 
     for await (let data of ws){
-        console.log(data, typeof data);
+        // console.log(data, typeof data);
+        const event = JSON.parse(data);
+        switch(event.event){
+            case 'join':
+                const userObj = {
+                    userId,
+                    name: event.name,
+                    groupName: event.groupName,
+                    ws
+                };
+                usersMap.set(userId, userObj);
+                const users = groupsMap.get(event.groupName) || [];
+                users.push(userObj);
+                groupsMap.set(event.groupName, users);
+
+                emitEvent(event.groupName);
+        }
     }
+}
+
+function emitEvent(groupName){
+    const users = groupsMap.get(groupName) || [];
+
+    for ( const user of users){
+        const event = {
+            event: 'users',
+            data: getDisplayUsers(groupName)
+        }
+        user.ws.send(JSON.stringify(event))
+    }
+}
+
+function getDisplayUsers(groupName){
+    const users = groupsMap.get(groupName) || [];
+    return users.map(u => {
+        return {userId: u.userId, name: u.name};
+    })
 }
