d5b24f2c5e5b20f3f72ab476c94ba58a81fae2d8
tanhongit <phuongtan12357nguyen@gmail.com>
2020-09-09 15:03:37 +0700

    change server to localhost:3000

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 import { listenAndServe } from "https://deno.land/std@0.67.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.67.0/http/file_server.ts";
 import { acceptWebSocket, acceptable } from "https://deno.land/std@0.67.0/ws/mod.ts";
 import chat from './chat.js';
 listenAndServe({ port: 3000 }, async req => {
+    let url = req.url;
+    const position = url.indexOf('?');
+    if(position > -1 ){
+        url = url.substring(0, position);
+    }
+    const path = `${Deno.cwd()}/public${url}`;
+    if (await fileExist(path)) {
+        const content = await serveFile(req, path);
+        req.respond(content);
+        return;
+    }
+
     if (acceptable(req)) {
         acceptWebSocket({
             conn: req.conn,
@@ -10,5 +23,18 @@ listenAndServe({ port: 3000 }, async req => {
             headers: req.headers
         }).then(chat);
     }
-})
+});
+
+async function fileExist(path) {
+    try {
+        const start = await Deno.lstat(path);
+        return start && start.isFile;
+    } catch (e) {
+        if (e && e instanceof Deno.errors.NotFound) {
+            return false;
+        } else {
+            throw e;
+        }
+    }
+}
 console.log('Server started on port 3000');
